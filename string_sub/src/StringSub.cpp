43094a5064948bfd2ccfc350f9af47d9df095aac
Kevin Lien <klien8589@gmail.com>
2017-11-13 22:45:24 -0800

    added pointer implementation

--- string_sub/src/StringSub.cpp
+++ string_sub/src/StringSub.cpp
@@ -4,48 +4,49 @@ StringSub::StringSub(const string& file) : bf(file) { }
 
 void StringSub::replace(const string& oldstring, const string& newstring) {
   char c;
-  unsigned int match = 0;
+  const char* tc = oldstring.c_str();
+
   while(bf.get(c)) {
     // if match is found // call replace function
-    if (c == oldstring[match]) {
-      ++match;
-      if (match == oldstring.size()) {
+    if (c == *tc) {
+      ++tc;
+      if (tc-oldstring.c_str() == (int)oldstring.size()) {
         // match a newstring.size() different depending on oldstring and new
         if (oldstring.size() >= newstring.size()) {
           for (unsigned int i = 0; i < newstring.size(); ++i) 
             bf.put(newstring[i]);
         } else {
-          for (unsigned int i = 0; i < match; ++i) {
+          for (unsigned int i = 0; i < tc-oldstring.c_str(); ++i) {
             td.push_back(newstring[i]);
             bf.put(td.front());
             td.pop_front();
           }
-          for (unsigned int i = match; i < newstring.size(); ++i) 
+          for (unsigned int i = tc-oldstring.c_str(); i < newstring.size(); ++i) 
             td.push_back(newstring[i]);
         }
-          match = 0;
+        tc = oldstring.c_str();
       }
     }
     else {
-      if (match > 0) {
-        unsigned int j = 0;
+      if (tc-oldstring.c_str() > 0) {
+        const char *j = oldstring.c_str();
         bf.put(oldstring[0]);
-        for (unsigned int i = 1; i < match; ++i) {
-          if (oldstring[i] == oldstring[j]) {
+        for (unsigned int i = 1; i < tc-oldstring.c_str(); ++i) {
+          if (oldstring[i] == *j) {
             j++;
           } else {
             // if during recalc match ends
-            if (j > 0) {
-              for (unsigned int k = 0; k < j; ++k)
+            if (j-oldstring.c_str() > 0) {
+              for (unsigned int k = 0; k < j-oldstring.c_str(); ++k)
                 bf.put(oldstring[k]);
             }
             bf.put(oldstring[i]);
-            j = 0;
+            j = oldstring.c_str();
           }
         }
-        if (c == oldstring[j]) j++;
-        else j = 0;
-        match = j;
+        if (c == *j) j++;
+        else j = oldstring.c_str();
+        tc = j;
       }
       // temp char that hold cur character or characer in deque
       char temp_char = c;
@@ -55,7 +56,7 @@ void StringSub::replace(const string& oldstring, const string& newstring) {
         td.push_back(c);
       }
       // if there is a match from recalculating, dont put
-      if (match == 0)
+      if (*tc == *oldstring.c_str())
         bf.put(temp_char);
     }
   }
